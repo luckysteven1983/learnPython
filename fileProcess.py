d10e8a75c0eeb0d62dfb37647161a63f9daa40a5
Da Li <Da.D.Li@alcatel-lucent.com>
2015-05-22 16:54:40 +0800

    continue

--- fileProcess.py
+++ fileProcess.py
@@ -1,6 +1,13 @@
 data = open('sketch.txt')
 for each_line in data:
-    (role, line_spoken) = each_line.split(':')
-    print(role),
-    print('said:'),
-    print(line_spoken),
+#    if each_line.find(':') >= 0:
+#    if not each_line.find(':') == -1:
+    try:
+        (role, line_spoken) = each_line.split(':', 1)
+        print(role),
+        print('said:'),
+        print(line_spoken),
+    except:
+        pass
+#        print('#### Catch exception here! ####')
+data.close()
