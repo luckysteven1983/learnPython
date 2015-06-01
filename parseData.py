5d60586b950f53537dda7cf8a36e63d326a4a073
Da Li <Da.D.Li@alcatel-lucent.com>
2015-06-01 14:22:12 +0800

    finish Chapter 6

--- parseData.py
+++ parseData.py
@@ -29,26 +29,42 @@ def sanitize(time_string):
 ##with open('sarah.txt') as saf:
 ##    data = saf.readline()
 ##sarah = data.strip().split(',')
+
+##def get_coach_data(filename):
+##    try:
+##        with open(filename) as f:
+##            data = f.readline()
+##        return(data.strip().split(','))
+##    except IOError as ioerr:
+##        print('File error: ' + str(ioerr))
+##        return(None)
+##sarah = get_coach_data('sarah2.txt')
+##sarah_data ={}
+##sarah_data['Name'] = sarah.pop(0)
+##sarah_data['DOB'] = sarah.pop(0)
+##sarah_data['Times'] = sarah
+##print(sarah_data['Name'] + "'s fastest times are:" +
+##      str(sorted(set([sanitize(t) for t in sarah_data['Times']]))[0:3]))
+
 def get_coach_data(filename):
     try:
         with open(filename) as f:
             data = f.readline()
-        return(data.strip().split(','))
+        templ = data.strip().split(',')
+        return({'Name':templ.pop(0),
+                'Dob':templ.pop(0),
+                'Times':str(sorted(set([sanitize(t) for t in templ]))[0:3])})
     except IOError as ioerr:
         print('File error: ' + str(ioerr))
         return(None)
+james = get_coach_data('james2.txt')
+print(james['Name'] + "'s fastest times are: " + james['Times'])
 
-sarah = get_coach_data('sarah2.txt')
 ##(sarah_name, sarah_dob) = sarah.pop(0), sarah.pop(0)
 ##print(sarah_name + "'s fastest times are: " +
 ##      str(sorted(set([sanitize(t) for t in sarah]))[0:3]) +
 ##      ", and his/her birthday is: " + sarah_dob)
-sarah_data ={}
-sarah_data['Name'] = sarah.pop(0)
-sarah_data['DOB'] = sarah.pop(0)
-sarah_data['Times'] = sarah
-print(sarah_data['Name'] + "'s fastest times are:" +
-      str(sorted(set([sanitize(t) for t in sarah_data['Times']]))[0:3]))
+
 
 ##clean_james = []
 ##clean_julie = []
