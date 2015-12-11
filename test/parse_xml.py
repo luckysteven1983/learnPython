695cfa0bb976ce397b1a3f8ec53b87ad6ceb0139
Da Li <Da.D.Li@alcatel-lucent.com>
2015-12-11 19:33:43 +0800

    automation

--- test/parse_xml.py
+++ test/parse_xml.py
@@ -5,6 +5,8 @@ Created on Dec 10, 2015
 #e.g. http://www.cnblogs.com/itech/archive/2011/01/06/1924972.html
 '''
 import os
+from xml.dom import minidom
+
 class ParseXml(object):
     '''
     classdocs
@@ -16,41 +18,42 @@ class ParseXml(object):
         Constructor
         '''
     def TestMiniDom(self):
-        from xml.dom import minidom
-        doc = minidom.parse("employees.xml")
-        
+        doc = minidom.parse("SDM_General_Data.xml")
         # get root element: <employees/>
         root = doc.documentElement
+        for node in root.childNodes:
+            if node.nodeType == node.ELEMENT_NODE:
+                print node.attributes.get('name')
         
         # get all children elements: <employee/> <employee/>
-        employees = root.getElementsByTagName("employee")
-        
-        for employee in employees:
-            print("-------------------------------------------")
-            # element name : employee
-            print (employee.nodeName)
-            # element xml content : <employee><name>windows</name><age>20</age></employee>
-            # basically equal to toprettyxml function
-            print (employee.toxml())
-            
-            nameNode = employee.getElementsByTagName("name")[0]
-            print (nameNode.childNodes)
-            print (nameNode.nodeName + ":" + nameNode.childNodes[0].nodeValue)
-            ageNode = employee.getElementsByTagName("age")[0]
-            print (ageNode.childNodes)
-            print (ageNode.nodeName + ":" + ageNode.childNodes[0].nodeValue)
-            
-            print("-------------------------------------------")
-            # children nodes :  \n is one text element
-            # [
-            # <DOM Text node "' \n    '">, 
-            # <DOM Element: name at 0xc9e490>, 
-            # <DOM Text node "'\n    '">, 
-            # <DOM Element: age at 0xc9e4f0>, 
-            # <DOM Text node "'\n  '">
-            # ] 
-            for n in employee.childNodes:
-                print (n)
+#         employees = root.getElementsByTagName("employee")
+#         print employees
+#         for employee in employees:
+#             print("-------------------------------------------")
+#             # element name : employee
+#             print (employee.nodeName)
+#             # element xml content : <employee><name>windows</name><age>20</age></employee>
+#             # basically equal to toprettyxml function
+#             print (employee.toxml())
+#              
+#             nameNode = employee.getElementsByTagName("name")[0]
+#             print (nameNode.childNodes)
+#             print (nameNode.nodeName + ":" + nameNode.childNodes[0].nodeValue)
+#             ageNode = employee.getElementsByTagName("age")[0]
+#             print (ageNode.childNodes)
+#             print (ageNode.nodeName + ":" + ageNode.childNodes[0].nodeValue)
+#              
+#             print("-------------------------------------------")
+#             # children nodes :  \n is one text element
+#             # [
+#             # <DOM Text node "' \n    '">, 
+#             # <DOM Element: name at 0xc9e490>, 
+#             # <DOM Text node "'\n    '">, 
+#             # <DOM Element: age at 0xc9e4f0>, 
+#             # <DOM Text node "'\n  '">
+#             # ] 
+#             for n in employee.childNodes:
+#                 print (n)
 
     def GenerateXml(self):
         import xml.dom.minidom
@@ -77,8 +80,8 @@ class ParseXml(object):
         
 
 parsexml = ParseXml()
-#parsexml.TestMiniDom()            
-parsexml.GenerateXml()
+parsexml.TestMiniDom()            
+#parsexml.GenerateXml()
 
 
 
--- test/test_parse_xml.py
+++ test/test_parse_xml.py
@@ -34,12 +34,36 @@ def get_tagname_other():
 
 def get_xpath_2():  
     #doc = lxml.etree.XML(input_xml_string)
-    lxml.etree.parse('data.xml')
-    doc = lxml.etree.ElementTree.__getattribute__()
+    doc = lxml.etree.parse('data.xml')
     doc.getroot()
-    for node in doc.xpath("//item/data[@version = $name]", name = "1.0"):  
+    print doc.getroot()
+    #for node in doc.xpath("//item/data[@version = $name]", name = "1.0"):  
+    for node in doc.xpath("//item/data"): 
         print (node, node.tag, (node.items()))
-    print doc.xpath
-                       
+    print doc.xpath("//item/data")
 
-get_xpath_2()
+def test_parse_xml():
+    dictvalue = {}
+    doc = lxml.etree.parse('SDM_General_Data.xml')
+    print doc.getroot().tag
+    for firstlevel in doc.getroot():
+        #print (firstlevel.tag, firstlevel.attrib)
+        for secondlevel in firstlevel:
+            for thirdlevel in secondlevel:
+                #for datakey in thirdlevel.keys():
+                    #print datakey
+                    #print (firstlevel.tag, secondlevel.tag, thirdlevel.tag, datakey, thirdlevel.get(datakey))
+                #print {(firstlevel.get('name'), secondlevel.get('name'), thirdlevel.get('name')) : thirdlevel.get('value')}
+                #datapath = '//'+firstlevel.tag+'[@name="%s"]/'+secondlevel.tag+'[@name="%s"]/'+thirdlevel.tag+'[@name="%s"]/@value'
+                datapath = '//'+firstlevel.tag+'[@name="%s"]/'+secondlevel.tag+'[@name="%s"]/'+thirdlevel.tag+'[@name="%s"]'
+                datavalue = datapath+'/@value'
+                test = doc.xpath(datavalue %(firstlevel.get('name'), secondlevel.get('name'), thirdlevel.get('name')))
+                print (firstlevel.get('name'), secondlevel.get('name'), thirdlevel.get('name'), test)
+                #dictvalue[(firstlevel.get('name'), secondlevel.get('name'), thirdlevel.get('name'))] = thirdlevel.get('value')
+#     print node.get('name')
+#     anotherkey = ('IP_conf', 'BE.NETWORK.NRG.1.ZONE.1_IPV6', 'GMPS.HOSTNAME', 'Private_VLAN1')
+#     print '\n'
+#     print 'another value of dictvalue is: '+dictvalue[anotherkey]+'!'
+
+test_parse_xml()
+#get_xpath_2()
