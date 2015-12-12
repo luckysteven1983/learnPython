553642a054603cad2359cc76afb7e51535957cde
Da Li <lida59462492@gmail.com>
2015-12-12 13:08:54 +0800

    automation

--- test/test_parse_xml.py
+++ test/test_parse_xml.py
@@ -45,23 +45,26 @@ def get_xpath_2():
 def test_parse_xml():
     dictvalue = {}
     doc = lxml.etree.parse('SDM_General_Data.xml')
+    print doc
     print doc.getroot().tag
     for firstlevel in doc.getroot():
         #print (firstlevel.tag, firstlevel.attrib)
         for secondlevel in firstlevel:
             for thirdlevel in secondlevel:
-                #for datakey in thirdlevel.keys():
-                    #print datakey
-                    #print (firstlevel.tag, secondlevel.tag, thirdlevel.tag, datakey, thirdlevel.get(datakey))
-                #print {(firstlevel.get('name'), secondlevel.get('name'), thirdlevel.get('name')) : thirdlevel.get('value')}
                 #datapath = '//'+firstlevel.tag+'[@name="%s"]/'+secondlevel.tag+'[@name="%s"]/'+thirdlevel.tag+'[@name="%s"]/@value'
                 datapath = '//'+firstlevel.tag+'[@name="%s"]/'+secondlevel.tag+'[@name="%s"]/'+thirdlevel.tag+'[@name="%s"]'
                 datavalue = datapath+'/@value'
                 test = doc.xpath(datavalue %(firstlevel.get('name'), secondlevel.get('name'), thirdlevel.get('name')))
-                print (firstlevel.get('name'), secondlevel.get('name'), thirdlevel.get('name'), test)
+                if firstlevel.get('name') == 'IP_conf' and secondlevel.get('name') == 'LDAP_Traffic_IPV4' and thirdlevel.get('name') == 'VLAN_ID':
+                    print 'help:'+str(thirdlevel.get('version'))+'!'
+                    thirdlevel.set('version', '2')
+                    print 'help:'+str(thirdlevel.get('version'))+'!'
                 #dictvalue[(firstlevel.get('name'), secondlevel.get('name'), thirdlevel.get('name'))] = thirdlevel.get('value')
+    print doc
+    doc.write('xmltree.xml') 
+
 #     print node.get('name')
-#     anotherkey = ('IP_conf', 'BE.NETWORK.NRG.1.ZONE.1_IPV6', 'GMPS.HOSTNAME', 'Private_VLAN1')
+#     anotherkey = ('IP_conf', 'BE.NETWORK.NRG.1.ZONE.1_IPV6', 'GMPS.HOSTNAME')
 #     print '\n'
 #     print 'another value of dictvalue is: '+dictvalue[anotherkey]+'!'
 
